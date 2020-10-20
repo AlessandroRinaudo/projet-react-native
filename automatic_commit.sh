77d099db23b42f998acb6b6e8a5f30fc1110f22a
Alessandro Rinaudo <>
2020-10-20 17:27:15 +0200

    script commit

---
+++ automatic_commit.sh
@@ -0,0 +1,14 @@
+
+git pull
+git add projet.Rmd
+printf "Voulez vous écrire un message? (o/n)\n"
+read -r messaggio
+if [ $messaggio == 'o' ]
+then
+   printf "Ecrire ici le message \n"
+   read -r messaggioVeroEProprio
+   git commit -m "${messaggioVeroEProprio}"
+else
+   git commit -m "dernière mise à jour"
+fi
+git push
