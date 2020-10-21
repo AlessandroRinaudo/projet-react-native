edab15b22bf9e08ab7ac447c2d34c8d7b0ab2f1b
sebastien <sebast-94@hotmail.fr>
2020-10-21 17:33:11 +0200

    message

--- screens/Restaurants.js
+++ screens/Restaurants.js
@@ -1,37 +1,91 @@
 import React from 'react';
-import {View , Text ,StyleSheet, Image} from 'react-native';
-// import BackgroundImage from '../components/BackgroundImage';
-import Button from "../components/Button";
-import { getResources } from '../services/apirest';
-// import {images} from '../Constants/Images';
+import {View , Text ,StyleSheet, Image, ImageBackground} from 'react-native';
+import Button from "../components/Button"
+import BackgroundImage from '../components/BackgroundImage';
+import {images} from '../constants/Images';
 
+export default function Recettes(props) {
+  return <BackgroundImage>
+        <View style= {styles.container}> 
+            <Text style= {styles.containerHomeTitle} > Restaurants partenaires</Text>
+            <Text style= {styles.containerHome} > Tous les restaurants partenaires avec le bateau de Thibault.</Text>
+            <Text style= {styles.containerHome} >06.63.99.99.78</Text>
+            <Text style= {styles.containerHome} >lebateaudethibauth@gmail.com </Text>
+            <Text style= {styles.containerHome} >www.facebook.com/lebateaudethibauth </Text>
+            
+            
+            <View style={styles.affichage}>
+            <Button style= {styles.containerButton}
+            navigation={props.navigation}
+            text="Bistrot des Gascons"
+            screen=""
+            image={images.poisson.uri}
+            />
 
+            
+            <Button style= {styles.containerButton}
+            navigation={props.navigation}
+            text="Les fous de L'île"
+            screen=""
+            image={images.ancre.uri}
+            />
 
-export default class Restaurants extends React.Component {
-     state = {
-         data : []
-     }
-     componentDidMount() {
-         getResources('Restaurants').then(response=> {
-             this.setState({data:response})
-         })
-     }
-render() {
-  return <View style= {styles.container}> 
-            <Text >Page Restaurants</Text>
-            <Button navigation={this.props.navigation}
-            text="Product List" screen="ProductList"
-            data={this.state.data}/>
-            <Button navigation={this.props.navigation}
-            text="Retour" screen="Home"/>
+            <Button style= {styles.containerButton}
+            navigation={props.navigation}
+            text="Bistrot Landais"
+            screen=""
+            image={images.restaurant.uri}
+            />
+
+            <Button style= {styles.containerButton}
+            navigation={props.navigation}
+            text="Villa 9-Trois"
+            screen=""
+            image={images.recette.uri}
+            />
+
+            <Button style= {styles.containerButton}
+            navigation={props.navigation}
+            text="Bistrot du Sommelier"
+            screen=""
+            image={images.poisson.uri}
+            />
+            
+            <Button style= {styles.containerButton}
+            navigation={props.navigation}
+            text="Devenez partenaire!"
+            screen=""
+            image={images.poisson.uri}
+            />
+            </View>
         </View>
-    }
+        </BackgroundImage>
 }
+
 const styles = StyleSheet.create({
     container : {
         flex : 1,
         color:'black',
         alignItems: 'center',
         justifyContent : 'center',
+       
     },
+    containerHomeTitle : {
+        fontSize:30,
+        color:'#fff',
+    } ,
+    containerButton : {
+        fontSize:30,
+        color:'#fff',
+        marginBottom:45
+    },
+    containerHome : {
+        
+    }, 
+    affichage : {
+        flexWrap: 'wrap',
+        flexDirection: 'row',
+        justifyContent: 'center',
+    },
+
 });
