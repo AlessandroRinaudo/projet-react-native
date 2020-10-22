bd334476a4d3eab168d49976a5c840d0b3f2188b
quentin <quentin.joubert28@gmail.com>
2020-10-22 16:43:15 +0200

    nouvel mise à jour de l'application react native

---
+++ components/ProductCarts.js
@@ -0,0 +1,30 @@
+export default class Products extends React.Component {
+    render ()
+    {
+        return <TouchableOpacity style={styles.container}>
+            <Image style={styles.image} 
+            source={images.poulpe.uri}></Image>
+            <Text>{this.props.item.name} </Text>
+            <Text style= {styles.text}>{this.props.item.price}€</Text>
+            </TouchableOpacity>
+    }
+}
+
+const styles = StyleSheet.create({
+    container : {
+        //flex : 1,
+        flexDirection: "row",
+        padding: 10,
+        margin: 5,
+        backgroundColor:"#bbb",
+        //justifyContent: 'space-between'
+    },
+    text : {
+        
+    },
+    image : {
+        marginRight: 10,
+        width:50,
+        height:50
+    },
+});
--- screens/Cart.js
+++ screens/Cart.js
@@ -1,15 +1,55 @@
 import React from 'react';
 import {View , Text ,StyleSheet, Image} from 'react-native';
+import BackgroundImage from '../components/BackgroundImage';
+import Button from "../components/Button";
+// import {images} from '../Constants/Images';
+import Product from '../components/Products'
+import Headers from "../components/Headers";
+import {connect} from 'react-redux';
+import {addProduct, removeProduct} from '../store/action/cartAction'
 
-export default class Cart extends React.Component {
+class Cart extends React.Component {
 render() {
-  return <View style= {styles.container}> 
-        <Text> Cart page</Text>
-    </View>
+    return <View style= {styles.container}> 
+                <BackgroundImage>
+                <Headers  navigation={this.props.navigation}></Headers>
+                    <Text style ={styles.displayProduit}> Modifier la quantite en tapant sur chaque produits </Text>
+                    <View style= {styles.buttons} >
+                    {this.props.products.map((value,index)=> {
+                        return <ProductCarts key={index} item={value}/>
+                    })}
+                    </View>
+                </BackgroundImage>
+            </View>
     }
 }
+
+const mapStateToProps = state => {
+    return {
+        products: state.cart.products
+    }
+};
+
+export default connect(mapStateToProps)(Cart);
+
 const styles = StyleSheet.create({
     container : {
         flex : 1,
+        //justifyContent : 'center',
+        justifyContent: 'space-between',
+        marginTop: 50,
     },
+    displayProduit: {
+        padding:10,
+        textAlign:"center",
+        backgroundColor: "#71c1fc",
+        height:50
+    },
+    buttons:{
+        flex:1,
+        marginTop:15,
+        flexDirection:"column",
+        opacity:0.5,
+        height:10
+    }
 });
