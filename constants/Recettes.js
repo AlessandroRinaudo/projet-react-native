6782f84996691a488a3b04cf640f95b13b4522f2
quentin <quentin.joubert28@gmail.com>
2020-10-22 15:04:55 +0200

    constante recette

--- constants/Recettes.js
+++ constants/Recettes.js
@@ -1,13 +1,7 @@
 export const recettes = [
     {
-        name : "Bar roti a laurier frais",
-        image : require("../assets/images/recettes/barRecette.png"),
-        image_icon : require("../assets/images/recettes/barRecette_icon.png"),
-        description : ["Sur une plaque ou un plat allant au four, disposer quelques feuilles de laurier frais, verser un filet d'huile d'olive et du gros sel. Disposer le bar, puis l'arroser d'un filet d'huile d'olive et mettre un peu de gros sel sur la peau. ",
-        "Cuire au four pendant 12 min à 240° C."],
-    },
-    {
-        name : "Homard en chaud-froid",
+        name : "Homard",
+        name_long : "Homard en chaud-froid",
         image : require("../assets/images/recettes/homardRecette.png"),
         image_icon : require("../assets/images/recettes/homardRecette_icon.png"),
         description : ["Faites cuire les homards dans de l'eau bouillante avec du thym, du laurier, du sel et du poivre de Cayenne. Laissez cuire 20 minutes. Égouttez-les et laissez-les refroidir.",
@@ -15,11 +9,28 @@ export const recettes = [
         "Mélangez la mayonnaise avec le cognac, le corail et la ciboulette ciselée."],
     },
     {
-        name : "Noix de Saint-Jaques flambé au cognac",
+        name : "St Jacques",
+        name_long : "Noix de Saint-Jaques flambé au cognac",
         image : require("../assets/images/recettes/saintJacques.png"),
         image_icon : require("../assets/images/recettes/saintJacques_icon.png"),
         description : ["Faire fondre du beurre avec des échalotes puis ajouter les noix de Saint-Jacques. Les faire revenir en laissant le milieu translucide puis les retirer du feu. ",
         "Ajouter l'ail et le persil dans la poêle et laisser cuire quelques secondes. Bien faire chauffer la poêle, puis flamber au Cognac. Une fois la flamme éteinte, ajouter les noix de Saint-Jacques (il ne faut pas les flamber car elles perdraient leur saveur).",
         "Déguster chaud nature ou accompagné d'une fondue de poireaux."],
+    },
+    {
+        name : "Bar",
+        name_long : "Bar roti a laurier frais",
+        image : require("../assets/images/recettes/barRecette.png"),
+        image_icon : require("../assets/images/recettes/barRecette_icon.png"),
+        description : ["Sur une plaque ou un plat allant au four, disposer quelques feuilles de laurier frais, verser un filet d'huile d'olive et du gros sel. Disposer le bar, puis l'arroser d'un filet d'huile d'olive et mettre un peu de gros sel sur la peau. ",
+        "Cuire au four pendant 12 min à 240° C."],
+    },
+    {
+        name : "Tourteau",
+        name_long : "Tourteau",
+        image : require("../assets/images/home/tourteau.png"),
+        image_icon : require("../assets/images/home/tourteau.png"),
+        description : ["",
+        ""],
     },
 ]
